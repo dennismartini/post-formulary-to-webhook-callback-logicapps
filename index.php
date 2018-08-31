021079704beb3a90c56e8959a807ea1b2f3f8b0f
dennis di martini ronn <dennis.Ronn@bematech.com.br>
2018-08-31 02:52:34 -0300

    Layoutit

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
  <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Area de aprovação!</title>
+    <meta name="author" content="LayoutIt!">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
 <body>
 <?php
 //<form method="post">
@@ -44,17 +53,22 @@ echo $server_output;
         <button name="button" style="background-color:#556270;border:1px solid #000000;border-radius:8px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:50px;font-weight:bold;line-height:80px;text-align:center;text-decoration:none;width:400px;-webkit-text-size-adjust:none;mso-hide:all;"  >Aprovar</button>
     </p>
     </form>
-<input type="text" name="Comments" /><br>
-<div class="container-fluid">
+    <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
+			 
 			<button type="button" class="btn btn-primary btn-lg">
 				Button
 			</button>
 			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
 			</p>
 		</div>
 	</div>
 </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html> 
